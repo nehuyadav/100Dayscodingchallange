efd72e64630fcba685780884fae8eb8fee260de9
nehuyadav <146457490+nehuyadav@users.noreply.github.com>
2023-10-02 00:57:32 +0530

    Add files via upload

---
+++ Day1_addnum.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class addsum{
+  public static void main (String[]args){
+   Scanner sc=new Scanner(System.in);
+System.out.println("enter any five numbers");
+System.out.println("enter first number");
+  int num1=sc.nextInt();
+System.out.println("enter second number");
+ int num2=sc.nextInt();
+System.out.println("enter third number");
+ int num3=sc.nextInt();
+System.out.println("enter four number");
+ int num4=sc.nextInt();
+System.out.println("enter five number");
+ int num5=sc.nextInt();
+System.out.println("sum="+(num1+num2+num3+num4+num5+);
+}
+}
