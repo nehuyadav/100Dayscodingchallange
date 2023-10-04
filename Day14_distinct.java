25ac9221205c547e81574a4002cc311dcf131de0
nehuyadav <146457490+nehuyadav@users.noreply.github.com>
2023-10-04 23:00:49 +0530

    Add files via upload

---
+++ Day14_distinct.java
@@ -0,0 +1,32 @@
+import java.util.*;
+class distinct
+{
+  public static void main(String[]args)
+{
+      System.out.println("enter marks of maths");
+      Scanner sc=new Scanner(System.in);
+       int marks1=sc.nextInt();
+      System.out.println("enter marks of c");
+      Scanner sc=new Scanner(System.in);
+       int marks2=sc.nextInt();
+      System.out.println("enter marks of c++");
+      Scanner sc=new Scanner(System.in);
+       int marks3=sc.nextInt();
+      System.out.println("enter marks of java");
+      Scanner sc=new Scanner(System.in);
+       int marks4=sc.nextInt();
+      System.out.println("enter marks of os");
+      Scanner sc=new Scanner(System.in);
+       int marks5=sc.nextInt();
+      if(mark1>=75)
+   System.out.println("i got distinct in maths with marks:"+marks1);
+      if(mark2>=75)
+   System.out.println("i got distinct in c with marks:"+marks2);
+      if(mark3>=75)
+   System.out.println("i got distinct in c++ with marks:"+marks3);
+       if(mark4>=75)
+   System.out.println("i got distinct in java with marks:"+marks4);
+       if(mark5>=75)
+   System.out.println("i got distinct in os with marks:"+marks5);
+    }
+}
