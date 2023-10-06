a22f90d00d85c96891ce674e81932f6f4eada063
nehuyadav <146457490+nehuyadav@users.noreply.github.com>
2023-10-06 20:45:08 +0530

    Add files via upload

---
+++ Day15_even.java
@@ -0,0 +1,13 @@
+
+class even {
+ public static void main(String[]args){
+ Scanner sc=new Scanner(System.in);
+int i;
+System.out.println("first 10 even number are such that-");
+for(i=1;i<10;i++)
+{
+   int n=i*2;
+   System.out.println(""+n));
+}
+}
+}
---
+++ Day15_odd.java
@@ -0,0 +1,12 @@
+
+class odd{
+ public static void main(String[]args){
+int i;
+System.out.println("first 10 even number are such that-");
+for(i=0;i<10;i++)
+{
+   int n=i*2+1;
+   System.out.println(""+n);
+}
+}
+}
