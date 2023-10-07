f105871cae68e85f8ecfe1afc189fa61433e14de
nehuyadav <146457490+nehuyadav@users.noreply.github.com>
2023-10-07 20:36:07 +0530

    Add files via upload

---
+++ Day16_naturalnum_5_sum.java
@@ -0,0 +1,10 @@
+class naturalnum_5sum{
+public static void main(String[]args){
+System.out.println("sum of 5 natural numbers\n");
+int i,sum=0;
+for(i=1;i<=5;i++)
+{
+    sum+=i;
+  
+} System.out.println(""+sum);
+}}
---
+++ Day16_table_5.java
@@ -0,0 +1,8 @@
+class table_5{
+public static void main(String[]args){
+int i;
+System.out.println("table of 5\n");
+for(i=1;i<=10;i++)
+{
+   System.out.println(""+(i*5));
+}}}
