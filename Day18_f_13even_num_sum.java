b3ed4bf48f49c3e28b6fe3a2151150f178cb34ae
nehuyadav <146457490+nehuyadav@users.noreply.github.com>
2023-10-09 19:39:39 +0530

    Add files via upload

---
+++ Day18_f_13even_num_sum.java
@@ -0,0 +1,13 @@
+class f_13even_num_sum{
+ public static void main(String[]args){
+System.out.println("first 13 even numbers sum");
+int i,n,sum=0;
+for(i=0;i<=12;i++)
+{
+  n= 2*i;
+sum+=n;
+
+}
+  
+   System.out.println(sum);
+ }}
