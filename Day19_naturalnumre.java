805bb3b76112e8fe4c6e36423550d4a6c5bf5f3f
nehuyadav <146457490+nehuyadav@users.noreply.github.com>
2023-10-10 18:46:02 +0530

    Add files via upload

---
+++ Day19_naturalnumre.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class naturalnumre{
+public static void main(String[]args)
+{
+System.out.println("enter a natural number form that you went to print reverse order"):
+Scanner sc= new Scanner(System.in);
+int n=sc.nextInt();
+  for(int i=n;i>=0;i--)
+ {
+     System.out.println(i);
+}}} 
+   
+    
